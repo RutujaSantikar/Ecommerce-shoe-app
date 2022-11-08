e62a7a0ad0098cd9b9bbb1d87524f3dfcbe73b7e
Rutuja Santikar <santikarrutuja@gmail.com>
2022-11-08 16:59:39 +0530

    navbar & homepage added

---
+++ src/components/AppHome.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="body">
+   
+   <div class="headline"> DISCOVER LIMITED SNEAKERS WITHOUT LIMITATIONS  </div>
+   <div class="padding"></div>
+   <div><button class="shopbtn">Shop Now</button></div>
+
+  </div>
+</template>
+
+<script>
+export default { 
+name:"AppHome"
+}
+</script>
+
+<style scoped>
+.body{
+  width: 90%;
+  margin: 0 auto;
+}
+.headline{
+  font-family: 'Anton', sans-serif;
+  font-size: 6.5em;
+  width: 100%;
+}
+.padding{
+  padding: 20px;
+}
+.shopbtn{
+  padding: 15px;
+  background-color: #d9d9d9;
+  color: black;
+  border: none;
+  font-weight:200;
+  font-family:'Anton', sans-serif;
+  letter-spacing: 0.3em;
+  width: 35%;
+  
+}
+.shopbtn:hover{
+  background-color: black;
+  color: white;
+}
+
+@media(max-width:1000px){
+  .headline{
+    font-size: 2.8em;
+  }
+  .shopbtn{
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="body">
+     <b-navbar
+      class="navbar bg-info  p-3 mb-5"
+      toggleable="lg"
+      variant="info"
+    >
+      <b-navbar-brand class="navbar-brand" href=""
+        >HYPERBIZZ</b-navbar-brand
+      >
+
+      <b-navbar-toggle
+        class="navbar-toggler"
+        target="nav-collapse"
+      ></b-navbar-toggle>
+        
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item class="nav-link " href="/#"
+            > <input type="search " placeholder="search brands or items" class="search-box sm-none"></b-nav-item>
+
+          <b-nav-item class="nav-link  " href="/#"
+            ><i class="fa-solid fa-user"></i></b-nav-item
+          >
+          <b-nav-item class="nav-link " href="/#"
+            ><i class="fa-solid fa-heart"></i></b-nav-item
+          >
+          <b-nav-item class="nav-link  " href="/#"
+            ><i class="fa-solid fa-bag-shopping"></i></b-nav-item
+          >
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+         <div class="align-right">
+          <button class="btn btn-signup  ">Sign up</button>
+          
+          <button class="btn btn-signin  ">Sign in</button>
+
+         </div>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppNavbar',
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.body{
+  width:90%;
+  margin: 0 auto;
+}
+.navbar{
+  background-color:white  !important;
+}
+.navbar-brand{
+  font-family: 'Anton', sans-serif;
+  font-size: 25px;
+}
+.align-right{
+  margin-left: 10em;
+}
+.nav-link{
+  margin: 0px 5px 0px 5px;
+}
+.navbar-toggler{
+  font-size: 14px;
+  color: #d9d9d9;
+}
+.btn{
+ margin: 0px 5px 0px 5px;
+  border-radius:15px;
+  }
+.btn:hover{
+  background-color: #d9d9d9;
+  
+}
+input{
+  margin-left: 10em;
+}
+
+@media (max-width:1000px){
+
+.sm-none{
+  display: none;
+}
+
+.align-right{
+  margin-left: 1em;
+}
+
+}
+</style>
